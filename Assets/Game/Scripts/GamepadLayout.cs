593418bf12a3a81fc7832a63f27ae025905f3122
Yasar Gencer <gncrysrr@gmail.com>
2025-05-08 00:30:19 +0300

    Add: Gamepad key stroke ui

---
+++ Assets/Game/Scripts/GamepadLayout.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GamepadLayout : MonoBehaviour
+{
+    [SerializeField] InputActionAsset inputActions;
+
+    [SerializeField] RectTransform leftStick, rightStick;
+    [SerializeField] RectTransform aButton, bButton;
+
+    void OnEnable()
+    {
+        inputActions.Enable();
+        inputActions.FindAction("Player/Move").performed += OnMove;
+        inputActions.FindAction("Player/Move").canceled += OnMove;
+        inputActions.FindAction("Player/Look").performed += OnLook;
+        inputActions.FindAction("Player/Look").canceled += OnLook;
+        inputActions.FindAction("Player/Jump").performed += OnJump;
+        inputActions.FindAction("Player/Jump").canceled += OnJump;
+    }
+    void OnDisable()
+    {
+        inputActions.Disable();
+        inputActions.FindAction("Player/Move").performed -= OnMove;
+        inputActions.FindAction("Player/Move").canceled -= OnMove;
+        inputActions.FindAction("Player/Look").performed -= OnLook;
+        inputActions.FindAction("Player/Look").canceled -= OnLook;
+        inputActions.FindAction("Player/Jump").performed -= OnJump;
+        inputActions.FindAction("Player/Jump").canceled -= OnJump;
+    }
+    void OnMove(InputAction.CallbackContext context)
+    {
+        if (InputManager.Instance.OnGamepad == false)
+            return;
+        Vector2 moveInput = context.ReadValue<Vector2>();
+        leftStick.anchoredPosition = moveInput * 20;
+    }
+    void OnLook(InputAction.CallbackContext context)
+    {
+        if (InputManager.Instance.OnGamepad == false)
+            return;
+        Vector2 lookInput = context.ReadValue<Vector2>();
+        rightStick.anchoredPosition = lookInput * 20;
+    }
+    void OnJump(InputAction.CallbackContext context)
+    {
+        if (InputManager.Instance.OnGamepad == false)
+            return;
+        if (context.performed)
+        {
+            aButton.gameObject.SetActive(true);
+        }
+        else if (context.canceled)
+        {
+            aButton.gameObject.SetActive(false);
+        }
+    }
+
+
+}
